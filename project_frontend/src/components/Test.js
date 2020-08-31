dc90fa679123bd70e372320a0d32d3462b480372
andrew041994 <andrew041994@gmail.com>
2020-08-30 22:06:54 -0400

    styled div

--- project_frontend/src/components/Test.js
+++ project_frontend/src/components/Test.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class test extends Component {
   render() {
     return (
-      <div> 
+      <div style={{"margin": "auto",
+      "width": "20%",
+      "padding": "10px",
+      "color": "white"}}> 
           
           <button> press here for action</button>
      </div>
