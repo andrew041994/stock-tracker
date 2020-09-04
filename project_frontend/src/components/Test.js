658c55aba2d0e3a02d7c6556036af05d835c7947
andrew041994 <andrew041994@gmail.com>
2020-09-03 22:52:34 -0400

    updated form

--- project_frontend/src/components/Test.js
+++ project_frontend/src/components/Test.js
@@ -17,10 +17,19 @@ handleClick = (e) => {
           
           <button onClick={(e) => this.handleClick(e)}> press here for action</button>
           <form>
+            <div>
+
           <label>
             Username
             <input id="username" name="username" type="text" value={this.state.username} onChange={event => this.handleUsername(event)}/>
           </label>
+            </div>
+          <div>
+          <label>
+            Password
+            <input id="password" name="password" type="password" value={this.state.password} onChange={event => this.handlePassword(event)} />
+          </label>
+        </div>
           </form>
      </div>
     );
